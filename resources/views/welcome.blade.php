6bd4bb5701d17411d295f2f33941f88833bec8df
BoydHalfman2003 <boyd.halfman@student.graafschapcollege.nl>
2024-10-14 15:19:37 +0200

    layouts

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="antialiased">
-    @include('shared.header')
+    <h1 class="text-[50px]">test</h1>
 </body>
 </html>
