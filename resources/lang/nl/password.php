94e3e87e089f8bc4a3dd973e6f5aa43cea03b4b6
yaelscholten <yaelscholten1@gmail.com>
2024-10-16 16:40:11 +0200

    Create password.php

---
+++ resources/lang/nl/password.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'reset' => 'Uw wachtwoord is opnieuw ingesteld!',
+    'sent' => 'We hebben uw wachtwoord reset-link per e-mail verzonden!',
+    'throttled' => 'Even geduld voordat u het opnieuw probeert.',
+    'token' => 'Dit wachtwoord reset-token is ongeldig.',
+    'user' => "We kunnen geen gebruiker vinden met dat e-mailadres.",
+];
