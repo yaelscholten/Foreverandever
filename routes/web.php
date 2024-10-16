cdccad5aafea1ceb50f575c6b67667554757b7a1
BoydHalfman2003 <boyd.halfman@student.graafschapcollege.nl>
2024-10-16 10:34:13 +0200

    Home

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/register', function () {
+    return view('register');
+});
 
 
-//Auth::routes();
-Route::get('/register', [RegisteredUserController::class, 'create'])->name('register');
+Auth::routes();
+Route::get('/register', [RegisteredUserController::class, 'create'])->name('auth.register');
 Route::post('/register', [RegisteredUserController::class, 'store']);
