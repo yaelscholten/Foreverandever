b361b531d043f7bac34065af0f00a9b431d4568a
BoydHalfman2003 <boyd.halfman@student.graafschapcollege.nl>
2024-10-16 15:05:43 +0200

    Inloggen etc styling

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,27 +6,30 @@
     </x-slot>
     <div class="w-full h-auto py-[3rem]">
         <div class="max-w-[80rem] px-[1rem] mx-auto">
-            <div class="w-full h-auto bg-white p-[2rem] rounded-xl flex justify-around gao-[2rem]">
-                <div>
+            <div class="w-full h-auto rounded-xl flex justify-between gao-[2rem]">
+                <div class="w-[12.5%]">
                     <h2 class="font-bold">Eventnaam</h2>
                 </div>
-                <div>
+                <div class="w-[12.5%]">
                     <h2 class="font-bold">Domeinnaam</h2>
                 </div>
-                <div>
+                <div class="w-[12.5%]">
                     <h2 class="font-bold">Geacepteerd</h2>
                 </div>
-                <div>
+                <div class="w-[12.5%]">
                     <h2 class="font-bold">Datum</h2>
                 </div>
-                <div>
+                <div class="w-[12.5%]">
                     <h2 class="font-bold">Online t/m</h2>
                 </div>
-                <div>
+                <div class="w-[12.5%]">
                     <h2 class="font-bold">Thema</h2>
                 </div>
-                <div>
+                <div class="w-[12.5%]">
                     <h2 class="font-bold">Status</h2>
+                </div>
+                <div class="w-[12.5%]">
+                    <h2 class="font-bold">Preview</h2>
                 </div>
             </div>
         </div>
