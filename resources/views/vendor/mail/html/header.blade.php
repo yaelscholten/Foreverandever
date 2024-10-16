b38aae50207576be40b3dad8f30491372ae5115f
yaelscholten <yaelscholten1@gmail.com>
2024-10-16 16:59:01 +0200

    Update header.blade.php

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,12 +1,13 @@
 @props(['url'])
+
 <tr>
-<td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-@else
-{{ $slot }}
-@endif
-</a>
-</td>
+    <td class="header" style="background-color: #ebfaec; width: 100%; height: 200px; text-align: center; padding: 20px;">
+        <a href="{{ $url }}" style="display: inline-block;">
+            @if (trim($slot) === 'Laravel')
+                <img src="{{ asset('fotos/login-imgage.jpg') }}" alt="Logo" style="width: 100px; height: auto;" />
+            @else
+                {{ $slot }}
+            @endif
+        </a>
+    </td>
 </tr>
