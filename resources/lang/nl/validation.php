e7bb97753f580e96af50bab23abfe40f501494b0
yaelscholten <yaelscholten1@gmail.com>
2024-10-16 11:20:32 +0200

    Vertaling

---
+++ resources/lang/nl/validation.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'required' => 'Het :attribute veld is verplicht.',
+    'email' => 'Het :attribute moet een geldig e-mailadres zijn.',
+    'max' => [
+        'string' => 'Het :attribute mag niet meer dan :max tekens bevatten.',
+    ],
+    'unique' => 'Het :attribute is al in gebruik.',
+    'confirmed' => 'De :attribute bevestiging komt niet overeen.',
+];
