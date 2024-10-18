03a5cf36423141f8f93ec8ec578b3e39b2e017f1
yaelscholten <yaelscholten1@gmail.com>
2024-10-18 12:43:32 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-
+    <script src="https://cdn.tailwindcss.com"></script>
     <style>
     .hero-video {
       position: absolute;
