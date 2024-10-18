29f96b7d6989494f29d8f9867167a439142d0dc1
yaelscholten <yaelscholten1@gmail.com>
2024-10-18 13:03:16 +0200

    cdn

--- resources/views/components/guest-layout.blade.php
+++ resources/views/components/guest-layout.blade.php
@@ -6,7 +6,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>{{ config('app.name', 'Laravel') }}</title>
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <script src="https://cdn.tailwindcss.com"></script>
+    
     </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="{{ asset('build/assets/app.css') }}">
-    <script src="{{ asset('build/assets/app.js') }}" defer></script>
+    <script src="https://cdn.tailwindcss.com"></script>    
     <style>
     .hero-video {
       position: absolute;
